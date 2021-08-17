6ec310902b7cea08a72165704fb4543748588416
GraViiX <lucladjevardi@gmail.com>
2021-08-17 14:50:00 +0200

    api toolbox update

--- Classes/Toolbox.py
+++ Classes/Toolbox.py
@@ -8,11 +8,39 @@ class apiBox(QObject):
     def __init__(self):
         QObject.__init__(self)
     
-    def postLogin(self):
-        pass
+    def postLogin(self, user, passw):
+        #API URL
+        url = 'http://192.168.4.231:48935/api/Users/Login'
+        #HEADERS
+        headers = {'content-type':'application/json'}
+        #BODY TO POST
+        body = {
+            'Username': user,
+            'Password': passw
+        }
+        #STRINGIFY JSON BODY FOR API
+        data=json.dumps(body, separators=(',',':'))
+        #REQUEST POST FOR LOGIN
+        r=requests.post(url=url, data=data, headers=headers)
+        #SAVE TOKEN
+        self.token = int(r.text)
+        #print(self.token) # USED FOR CHECKING # SAVE FOR LATER
 
     def getFriends(self):
-        pass
+        tknId = self.token
+        print(tknId)
+        self.fList=[]
+        #API URL
+        url = 'http://192.168.4.231:48935/api/FriendsList/Friends/'+str(tknId)
+        #HEADERS
+        headers = {'content-type':'application/json'}
+        #REQUEST GET FOR FRIENDSLIST
+        r=requests.get(url=url, headers=headers)
+        r=json.loads(r.text)
+        #print(r)
+        self.fList.clear()
+        self.fList.append(r)
+        #print(self.fList)
 
     def getPendigRequest(self):
         pass
